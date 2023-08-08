2d64301518deebca254fda6a6b576fcf96a64184
YesCoach <sbsii1020@gmail.com>
2023-08-08 15:35:02 +0900

    feat: API 관련 실습 진행 및 인증키 ignore 처리

--- SeSAC3Week4/ViewController.swift
+++ SeSAC3Week4/ViewController.swift
@@ -9,28 +9,71 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+struct Movie {
+    var title: String
+    var release: String
+}
+
+final class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+
+    var movieList: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.rowHeight = 60
+        tableView.dataSource = self
+        tableView.delegate = self
+
         callRequest()
     }
 
     func callRequest() {
-
-        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230806"
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.boxOfficeKey)&targetDt=20230806"
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
+
+                for item in json["boxOfficeResult"]["dailyBoxOfficeList"].arrayValue {
+                    let title = item["movieNm"].stringValue
+                    let release = item["openDt"].stringValue
+
+                    let movie = Movie(title: title, release: release)
+
+                    self.movieList.append(movie)
+                }
+
+                self.tableView.reloadData()
+
             case .failure(let error):
                 print(error)
             }
         }
-
     }
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell")!
+        cell.textLabel?.text = movieList[indexPath.row].title
+        cell.detailTextLabel?.text = movieList[indexPath.row].release
+
+        return cell
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
