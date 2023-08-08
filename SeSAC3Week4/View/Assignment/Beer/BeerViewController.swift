56b891b8d1db34e7eac5a3facba388493ba0be93
YesCoach <sbsii1020@gmail.com>
2023-08-08 17:59:42 +0900

    refactor: BeerViewController UI 속성 일부 수정

--- SeSAC3Week4/View/Assignment/Beer/BeerViewController.swift
+++ SeSAC3Week4/View/Assignment/Beer/BeerViewController.swift
@@ -23,20 +23,29 @@ final class BeerViewController: UIViewController {
 }
 
 private extension BeerViewController {
+
     func configureUI() {
         headerLabel.text = "오늘은 이 맥주를 추천합니다!"
         headerLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        headerLabel.textAlignment = .center
 
         imageView.contentMode = .scaleAspectFill
 
         nameLabel.font = .systemFont(ofSize: 14, weight: .bold)
+        nameLabel.textAlignment = .center
 
         descriptionLabel.font = .systemFont(ofSize: 13, weight: .regular)
         descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
 
-        beerButton.setTitle("다른 맥주 추천받기", for: .normal)
-        beerButton.setImage(.init(systemName: "star.bubble"), for: .normal)
-        beerButton.tintColor = .systemOrange
+        var config = UIButton.Configuration.plain()
 
+        config.title = "다른 맥주 추천받기"
+        config.image = .init(systemName: "star.bubble")
+        config.imagePadding = 8.0
+        config.imagePlacement = .leading
+        config.baseForegroundColor = .systemOrange
+
+        beerButton.configuration = config
     }
 }
