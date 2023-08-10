136395d2714432a35bed3890768e797571b6d6c6
YesCoach <sbsii1020@gmail.com>
2023-08-10 14:41:58 +0900

    feat: 언어감지 API 추가 및 관련 기능 구현

--- SeSAC3Week4/View/Practice/TranslationViewController.swift
+++ SeSAC3Week4/View/Practice/TranslationViewController.swift
@@ -15,21 +15,60 @@ class TranslationViewController: UIViewController {
     @IBOutlet var translateTextView: UITextView!
     @IBOutlet var requestButton: UIButton!
 
+    private var languageCode: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
 
     @IBAction func didRequestButtonTouched(_ sender: UIButton) {
+        fetchLanguageData()
+    }
 
+}
+
+private extension TranslationViewController {
+
+    func configureUI() {
+        originalTextView.text = nil
+        translateTextView.text = nil
+        translateTextView.isEditable = false
+    }
+
+    func fetchLanguageData() {
+        let url = "https://openapi.naver.com/v1/papago/detectLangs"
+        let header: HTTPHeaders = [
+            "X-Naver-Client-Id": APIKey.naverId,
+            "X-Naver-Client-Secret": APIKey.naverSecret
+        ]
+        let param: Parameters = [
+            "query": originalTextView.text!
+        ]
+        AF.request(url, method: .post, parameters: param, headers: header)
+            .validate()
+            .responseJSON { [weak self] response in
+             switch response.result {
+             case .success(let value):
+                 let json = JSON(value)
+                 let langCode = json["langCode"].stringValue
+                 self?.languageCode = langCode
+                 self?.fetchTranslationData(with: langCode)
+             case .failure(let error):
+                 print(error)
+             }
+         }
+    }
+
+    func fetchTranslationData(with langCode: String) {
         let url = "https://openapi.naver.com/v1/papago/n2mt"
         let header: HTTPHeaders = [
             "X-Naver-Client-Id": APIKey.naverId,
             "X-Naver-Client-Secret": APIKey.naverSecret
         ]
         let param: Parameters = [
-            "source": "ko",
-            "target": "ja",
+            "source": langCode,
+            "target": "ko",
             "text": originalTextView.text!
         ]
 
@@ -44,15 +83,4 @@ class TranslationViewController: UIViewController {
             }
         }
     }
-
-}
-
-private extension TranslationViewController {
-
-    func configureUI() {
-        originalTextView.text = nil
-        translateTextView.text = nil
-        translateTextView.isEditable = false
-    }
-
 }
